22afcff4f386cb080ec88536bf9bb505f2f70c99
Brendan Moor <bmoor9027@students.mc3.edu>
2023-05-10 15:42:23 -0400

    Week 13 Assignment

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,5 +1,29 @@
-const movies = [
+import poster1 from "../images/poster1.jpg"
+import poster2 from "../images/poster2.jpg"
+import poster3 from "../images/poster3.jpg"
 
-]
+const movies = [
+    {
+        id: 'm1',
+        poster: 'poster1',
+        title: 'The Unbearable Weight of Massive Talent',
+        release: '2022-4-22',
+        rating: 7,
+    },
+    {
+        id: 'm2',
+        poster: 'poster2',
+        title: 'The Northman',
+        release: '2022-4-22',
+        rating: 7,
+    },
+    {
+        id: 'm3',
+        poster: 'poster3',
+        title: 'Doctor Strange in the Multiverse of Madness',
+        release: '2022-5-6',
+        rating: 6.9,
+    },
+];
 
 export default movies;
